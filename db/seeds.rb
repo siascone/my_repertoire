c0619b8db6a868490be669a63c1ee92988f6d1b8
siascone <spencer.iascone@gmail.com>
2021-06-10 13:09:43 -0600

    Add a few post seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.delete_all
+Post.delete_all
 
 u1 = User.create!(
   fname: 'Guest',
@@ -22,4 +23,30 @@ u2 = User.create!(
   email: 'user.usington@user.com',
   username: 'username',
   password: 'password'
+)
+
+post1 = Post.create!(
+  title: 'The Post with the Most',
+  text: 'So much to say about this post...',
+  user_id: u1.id
+)
+post2 = Post.create!(
+  title: 'Painting landscapes',
+  text: 'My top tips to painting lanscapes are...',
+  user_id: u1.id
+)
+post3 = Post.create!(
+  title: 'Music for the soul',
+  text: 'Here is my favorite 10 albums of the 2010s...',
+  user_id: u1.id
+)
+post4 = Post.create!(
+  title: 'Music rocks',
+  text: 'Exploring my favorite styles of music...',
+  user_id: u2.id
+)
+post5 = Post.create!(
+  title: 'Theater Folks!',
+  text: 'Hey theater friends, I\'v set up a meetup for us...',
+  user_id: u2.id
 )
